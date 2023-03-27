2a6a2cd772313d06bfd86d0631acefa91e618ddb
Navdesh Singh Saini <120104452+Navdeshsinghsaini@users.noreply.github.com>
2023-03-27 12:25:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 //complete this code
-class Person {}
+class Person {
+	constructor() {
+		this.name = "Navdesh"
+	}
+	get getname () {
+		return (this.name);
+	}
+	set setage (){
+		this.age = "20";
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+   study() {
+	   console.log(`${this.name} is studying`);
+   }	 
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach() {
+		console.log(`${this.name} is teaching`);
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
